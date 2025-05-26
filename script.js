ec931ade24fbe70ebf9ece0a2defdb15af0d0883
Raunak Malhotra <rm1273503@gmail.com>
2025-05-26 11:58:37 +0530

    drag and drop done

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
+let draggedCard = null;
+
 function createTaskElement(taskText){
     const taskElement = document.createElement('div');
+    taskElement.setAttribute('draggable', true);
+    taskElement.addEventListener('dragstart', dragStart);
+    taskElement.addEventListener('dragend', dragEnd);
     taskElement.textContent = taskText;
     taskElement.classList.add('card');
     return taskElement;
@@ -18,3 +23,24 @@ function addTask(columnId){
     document.getElementById(`${columnId}-tasks`).appendChild(taskElement);
     input.value = "";
 }
+
+function dragStart(){
+    this.classList.add('dragging');
+    draggedCard = this;
+}
+function dragEnd(){
+    this.classList.remove('dragging');
+    draggedCard = null;
+}
+
+function dragOver(event){
+    event.preventDefault();
+    this.appendChild(draggedCard);
+}
+
+
+const columns = document.querySelectorAll('.column .tasks');
+
+columns.forEach((column) => {
+    column.addEventListener('dragover', dragOver)
+})
